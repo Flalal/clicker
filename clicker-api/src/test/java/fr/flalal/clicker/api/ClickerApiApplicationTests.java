a7c6fc022a8b58fbcf5aa555a867c4c6bf2bc8c5
Florian Flahaut <fflahaut@partner.auchan.fr>
2019-09-19 12:37:52 +0200

    feat: init project

---
+++ clicker-api/src/test/java/fr/flalal/clicker/api/ClickerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.flalal.clicker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
